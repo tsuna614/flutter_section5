671d23e70176b30055c54854968d2f65313011db
Khanh <21520982@ms.uit.edu.vn>
2023-04-20 21:56:45 +0700

    first commit: section5-96

---
+++ lib/expenses.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [Text("The chart"), Text("Expenses list")])),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import './expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(home: Expenses()),
+  );
+}
