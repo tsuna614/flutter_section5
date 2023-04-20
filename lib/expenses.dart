7a5cd42d336a51f2c39d8972ab6d6db14fb5ad58
Khanh <21520982@ms.uit.edu.vn>
2023-04-21 00:07:56 +0700

    checkpoint section-5.99

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -8,13 +9,27 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    // Expense not to be confused with Expenses
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [Text("The chart"), Text("Expenses list")])),
+              children: const [Text("Hello world")])),
     );
   }
 }
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,1 +1,20 @@
+import 'package:uuid/uuid.dart';
 
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work } // enum creates a custom data type
+
+class Expense {
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category; // Category is the custom data type created above
+}
