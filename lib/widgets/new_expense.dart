e92cf8fefc3e916a4cd5a993946d393020aa8144
Khanh <21520982@ms.uit.edu.vn>
2023-04-21 17:51:23 +0700

    checkpoint section-5.110: added new TextField for amount and a "Cancel" button

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,11 +8,16 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var enteredTitle = '';
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  // dung de quan ly text user input
 
-  void _saveTitleInput(String inputValue) {
-    enteredTitle = inputValue;
-  }
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  } // void dispose la mot phan cua flutter widget lifecycle
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +25,35 @@ class _NewExpenseState extends State<NewExpense> {
       padding: const EdgeInsets.all(16),
       child: Column(children: [
         TextField(
-          onChanged: _saveTitleInput,
+          controller: _titleController,
           maxLength: 50,
           decoration: const InputDecoration(
             label: Text('Title'),
           ),
         ),
+        TextField(
+          controller: _amountController,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            prefixText:
+                '\$ ', // text nay se tu dong them vao khi nguoi dung click vao TextField, tuy nhien se khong duoc luu lai
+            label: Text('Amount'),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
         Row(
           children: [
             ElevatedButton(
               onPressed: () {
-                print(enteredTitle);
+                print(_titleController.text);
+                print(_amountController.text);
               },
               child: const Text('Save Expense'),
             ),
+            const Spacer(),
+            TextButton(onPressed: () {}, child: const Text('Cancel')),
           ],
         ),
       ]),
