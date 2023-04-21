90ee2ca3c136621fac6a4b090e225e0235518dba
Khanh <21520982@ms.uit.edu.vn>
2023-04-21 17:34:40 +0700

    checkpoint section-5.107: added appbar added IconButton inside Appbar, that trigger a showModalBottomSheet added new_expense.dart, that gets called when showModalBotoomSheet gets triggered added a TextField inside new_expense.dart

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/expense.dart';
 import 'expenses_list/expenses_list.dart';
+import './new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -24,9 +25,24 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+            "Flutter Expense Tracker",
+          ),
+          actions: [
+            IconButton(
+              onPressed: _openAddExpenseOverlay,
+              icon: const Icon(Icons.add),
+            ),
+          ]),
       body: Column(
         children: [
           const Text("Hello world"),
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  var enteredTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    enteredTitle = inputValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(children: [
+        TextField(
+          onChanged: _saveTitleInput,
+          maxLength: 50,
+          decoration: const InputDecoration(
+            label: Text('Title'),
+          ),
+        ),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                print(enteredTitle);
+              },
+              child: const Text('Save Expense'),
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
+}
