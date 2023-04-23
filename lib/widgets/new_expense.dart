d8766571a6d118cc2533c66b651c4e8d029b6198
Khanh <21520982@ms.uit.edu.vn>
2023-04-23 10:41:45 +0700

    checkpoint section-5.114: added DropdownButton

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/expense.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -10,18 +11,28 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
-  // dung de quan ly text user input
 
-  void _presentDatePicker() {
+  Category? _selectedCategory = Category.leisure;
+
+  // dung de quan ly text user input
+  DateTime?
+      _selectedDate; // add a question mark ? to make the variable nullable
+  // alternative: late DateTime _selectedDate;
+
+  void _presentDatePicker() async {
     final initialDate = DateTime.now();
     final firstDate = DateTime(2000, 1, 1);
     final lastDate =
-        DateTime(initialDate.year + 10, initialDate.month, initialDate.day);
-    showDatePicker(
+        DateTime(initialDate.year + 100, initialDate.month, initialDate.day);
+    final pickedDate = await showDatePicker(
         context: context,
         initialDate: initialDate,
         firstDate: firstDate,
         lastDate: lastDate);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+    print(pickedDate);
   }
 
   @override
@@ -35,70 +46,104 @@ class _NewExpenseState extends State<NewExpense> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: Column(children: [
-        TextField(
-          controller: _titleController,
-          maxLength: 50,
-          decoration: const InputDecoration(
-            label: Text('Title'),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
           ),
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: _amountController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  prefixText:
-                      '\$ ', // text nay se tu dong them vao khi nguoi dung click vao TextField, tuy nhien se khong duoc luu lai
-                  label: Text('Amount'),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText:
+                        '\$ ', // text nay se tu dong them vao khi nguoi dung click vao TextField, tuy nhien se khong duoc luu lai
+                    label: Text('Amount'),
+                  ),
                 ),
               ),
-            ),
-            // TextField khi duoc cho vao children cua Row phai bao trong Extended neu khong se bi loi (yea i don't get it either)
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                // align the contents to the right
-                crossAxisAlignment: CrossAxisAlignment.center,
-                // center the contents vertically
-                children: [
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Text('Selected Date'),
-                  IconButton(
-                    onPressed: _presentDatePicker,
-                    icon: const Icon(Icons.calendar_month),
-                  )
-                ],
+              // TextField khi duoc cho vao children cua Row phai bao trong Extended neu khong se bi loi (yea i don't get it either)
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  // align the contents to the right
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  // center the contents vertically
+                  children: [
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text((_selectedDate == null)
+                        ? "No date selected"
+                        : formatter.format(_selectedDate!)),
+                    // exclamation mark force dart to assume the variable to not be NULL
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              const SizedBox(
+                width: 10,
               ),
-            )
-          ],
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Row(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                print(_titleController.text);
-                print(_amountController.text);
-              },
-              child: const Text('Save Expense'),
-            ),
-            const Spacer(),
-            TextButton(
+              DropdownButton(
+                  value:
+                      _selectedCategory, // this will show a temporary category on the dropdown, instead of blank
+                  items: Category.values.map((data) {
+                    return DropdownMenuItem(
+                      value:
+                          data, // pass the data into the value, which will be called in the onChanged function
+                      child: Text(data.name.toUpperCase()),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    if (_selectedCategory == null) {
+                      return; // if it's null return nothing
+                    }
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
                 onPressed: () {
-                  Navigator.pop(context);
-                  // close context (co nghia la close nguyen Widget build(BuildContext context))
+                  print(_titleController.text);
+                  print(_amountController.text);
                 },
-                child: const Text('Cancel')),
-          ],
-        ),
-      ]),
+                child: const Text('Save Expense'),
+              ),
+              const Spacer(),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    // close context (co nghia la close nguyen Widget build(BuildContext context))
+                  },
+                  child: const Text('Cancel')),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
