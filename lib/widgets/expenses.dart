5b1ecb839b2886e6b41e57db97617f2046c3d140
Khanh <21520982@ms.uit.edu.vn>
2023-04-24 14:05:55 +0700

    checkpoint section-5.119: added dismissible to items in expenses_list

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -41,6 +41,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,8 +62,12 @@ class _ExpensesState extends State<Expenses> {
           ]),
       body: Column(
         children: [
-          const Text("Hello world"),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          const Text("The chart"),
+          Expanded(
+              child: ExpensesList(
+            expenses: _registeredExpenses,
+            onDismissExpense: _removeExpense,
+          )),
         ],
       ),
     );
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,19 +1,28 @@
+import 'dart:math';
+
 import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/expense.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onDismissExpense});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onDismissExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
-          key: ValueKey(index), child: ExpenseItem(expenses[index])),
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onDismissExpense(expenses[index]);
+        },
+        child: ExpenseItem(expenses[index]),
+      ),
     );
   }
 }
